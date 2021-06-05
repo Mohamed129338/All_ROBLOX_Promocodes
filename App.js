d2da84c27ecd79dbb7b8e464f551bbb5da6c2fec
Mohamed129338 <81376424+Mohamed129338@users.noreply.github.com>
2021-06-05 21:44:40 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+const home = document.getElementById("home")
+const codes = document.getElementById("codes")
+const about_me = document.getElementById("about-me")
+const why_i_made_this = document.getElementById("why-I-Made-This")
+
+home.onclick = () =>{
+    location.href="index.html?"
+}
+codes.onclick = () =>{
+    location.href="codes.html?"
+}
+about_me.onclick = () =>{
+    location.href="about-me.html?"
+}
+why_i_made_this.onclick = () =>{
+    location.href="why-i-made-this.html?"
+}
+
+
+
+
+
